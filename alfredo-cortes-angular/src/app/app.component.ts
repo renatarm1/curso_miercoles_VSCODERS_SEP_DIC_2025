dfe08195aade6e65ac25fed8e83a659c0b973c5f
AlfredoCorvi <al.cor.vi05@gmail.com>
2025-10-08 19:37:18 -0600

    rutas agregadas y home

--- alfredo-cortes-angular/src/app/app.component.ts
+++ alfredo-cortes-angular/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { RouterModule } from '@angular/router';
 import { ProfileComponent } from './profile/profile.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ProfileComponent],
+  imports: [RouterOutlet, ProfileComponent, RouterModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
