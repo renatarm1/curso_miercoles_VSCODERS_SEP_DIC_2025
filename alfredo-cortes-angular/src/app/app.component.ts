e30a31813b421ce7191fe6626e338faca9f7a1d7
AlfredoCorvi <al.cor.vi05@gmail.com>
2025-10-08 18:17:48 -0600

    creacion de componente profile

--- alfredo-cortes-angular/src/app/app.component.ts
+++ alfredo-cortes-angular/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ProfileComponent } from './profile/profile.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ProfileComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
